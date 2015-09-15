4eb8a0d6f004f5b1be86d5d2cba6221070a8f276
LongGang <lgpang@LongGangs-MacBook-Pro.local>
2015-09-16 00:50:15 +0200

    add unittest for rootfinding and minmod4

---
+++ pyvisc/tests/test_kernel_helper.py
@@ -0,0 +1,98 @@
+import pyopencl as cl
+from pyopencl import array
+import numpy as np
+from time import time
+import os
+
+import unittest
+
+class TestHelper(unittest.TestCase):
+  def setUp(self):
+    os.environ['PYOPENCL_CTX'] = ':1'
+    os.environ['PYOPENCL_COMPILER_OUTPUT']='1'
+
+    self.ctx = cl.create_some_context()
+    self.queue = cl.CommandQueue(self.ctx)
+
+  def test_minmod(self):
+    cwd, cwf = os.path.split(__file__)
+
+    kernel_src = """
+    #include "helper.h"
+    
+    __kernel void minmod_test(global real4 * result) {
+      int gid = (int) get_global_id(0);
+      if ( gid == 0 ) {
+      	   real4 a = (real4)(1.0f, -2.0f, -1.0f, 4.0f);
+      	   real4 b = (real4)(2.0f, 0.0f, -2.0f, 4.0f);
+      	   real4 c = (real4)(3.0f, 2.0f, -3.0f, 4.0f);
+           real4 d = minmod4(minmod4(a, b), c);
+           result[0] = d;
+      }
+    }
+    """
+    compile_options = ['-I %s'%os.path.join(cwd, '..', 'kernel')]
+    compile_options.append('-D USE_SINGLE_PRECISION')
+    compile_options.append('-D EOSI')
+
+    prg = cl.Program(self.ctx, kernel_src ).build(compile_options)
+   
+    final = np.empty(1).astype( array.vec.float4 )
+    mf = cl.mem_flags
+    final_gpu = cl.Buffer(self.ctx, mf.READ_WRITE, final.nbytes)
+    
+    prg.minmod_test(self.queue, (1,), None, final_gpu)
+    
+    cl.enqueue_read_buffer(self.queue, final_gpu, final).wait()
+
+    self.assertAlmostEqual(final[0][0], 1.0)
+    self.assertAlmostEqual(final[0][1], 0.0)
+    self.assertAlmostEqual(final[0][2], -1.0)
+    self.assertAlmostEqual(final[0][3], 4.0)
+    print 'minmod4 test pass'
+
+  def test_rootfinding(self):
+    cwd, cwf = os.path.split(__file__)
+
+    kernel_src = """
+    #include "helper.h"
+    
+    __kernel void rootfinding_test(global real * result) {
+      int gid = (int) get_global_id(0);
+      if ( gid == 0 ) {
+           real eps = 0.00001f;
+           real pre = P(eps);
+           real4 umu = (real4)(1.0f, 0.3f, 0.2f, 0.1f);
+           real u0 = gamma(umu.s1, umu.s2, umu.s3);
+           umu = u0*umu;
+           real4 T0m = (eps+pre)*umu[0]*umu - pre*gmn[0];
+           real M = sqrt(T0m.s1*T0m.s1 + T0m.s2*T0m.s2 + T0m.s3*T0m.s3);
+           real K2 = M*M;
+           real T00 = T0m.s0;
+           real ed_found;
+           rootFinding(&ed_found, &T00, &K2);
+           result[0] = ed_found;
+      }
+    }
+    """
+    compile_options = ['-I %s'%os.path.join(cwd, '..', 'kernel')]
+    compile_options.append('-D USE_SINGLE_PRECISION')
+    compile_options.append('-D EOSI')
+
+    prg = cl.Program(self.ctx, kernel_src ).build(compile_options)
+   
+    final = np.empty(1).astype(np.float32)
+    mf = cl.mem_flags
+    final_gpu = cl.Buffer(self.ctx, mf.READ_WRITE, final.nbytes)
+    
+    prg.rootfinding_test(self.queue, (1,), None, final_gpu)
+    
+    cl.enqueue_read_buffer(self.queue, final_gpu, final).wait()
+
+    self.assertAlmostEqual(final[0], 0.00001)
+    print 'rootfinding test pass'
+
+
+
+if __name__ == '__main__':
+    unittest.main()
