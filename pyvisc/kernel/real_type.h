e292b7be7e44c6a8a96d426c51de96260bca666e
LongGang Pang <lgpang@qq.com>
2015-10-28 17:29:23 +0100

    use acu=1.0E-7 for float precision

--- pyvisc/kernel/real_type.h
+++ pyvisc/kernel/real_type.h
@@ -6,7 +6,7 @@ typedef float  real;
 typedef float4 real4;
 typedef float8 real8;
 typedef float3 real3;
-#define acu 1.0E-14f
+#define acu 1.0E-7f
 #else
 #pragma OPENCL EXTENSION cl_khr_fp64 : enable                  
 typedef double  real;
