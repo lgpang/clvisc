82f6d5f53576fa2fa99c73db44945c4dc91fd998
LongGangPang <lpang@lxlcsc0001.gsi.de>
2015-11-21 17:48:59 +0100

    compare with volodymyr for glueball eos

--- pyvisc/glueball.py
+++ pyvisc/glueball.py
@@ -8,15 +8,15 @@ from eos.eos import Eos
 from time import time
 import os
 from subprocess import call
+from config import cfg, write_config
 
 def glueball(Tmax = 0.6, outdir = '../results/event0'):
-    from config import cfg
     print('start ...')
     t0 = time()
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    cfg.IEOS = 2
+    cfg.IEOS = 3
     eos = Eos(cfg.IEOS)
     # update the configuration
     cfg.Edmax = eos.f_ed(Tmax)
@@ -24,13 +24,21 @@ def glueball(Tmax = 0.6, outdir = '../results/event0'):
 
     # set IEOS = 2 for (2+1)-flavor QCD EOS
     # set IEOS = 3 for GlueBall EOS
-    cfg.NX = 301
-    cfg.NY = 301
-    cfg.NZ = 81
-    cfg.DT = 0.02
-    cfg.DX = 0.16
-    cfg.DY = 0.16
+    cfg.NX = 401
+    cfg.NY = 401
+    cfg.NZ = 1
+    cfg.DT = 0.01
+    cfg.DX = 0.08
+    cfg.DY = 0.08
+    cfg.A = 208
+    cfg.Ra = 6.62
+    cfg.Eta = 0.546
+    cfg.Si0 = 6.4
+    cfg.TAU0 = 0.5
     cfg.ImpactParameter = 3.0
+    cfg.ETAOS = 0.0
+    cfg.SQRTS = 2760
+    write_config(cfg)
 
     ideal = CLIdeal(cfg)
     from glauber import Glauber
@@ -44,14 +52,7 @@ def glueball(Tmax = 0.6, outdir = '../results/event0'):
 
 
 if __name__=='__main__':
-    path = '/scratch/hyihp/pang/ini/GlueBall/'
-    glueball(0.60, path+'QCD_AATmax0p6_b7fm')
-    #glueball(0.45, path+'AATmax0p45_b7fm')
-    #glueball(0.3, path+'AATmax0p3_b7fm')
-    #glueball(0.55, path+'AATmax0p55')
-    #glueball(0.50, path+'AATmax0p5')
-    #glueball(0.45, path+'AATmax0p45')
-    #glueball(0.40, path+'AATmax0p4')
-    #glueball(0.35, path+'AATmax0p35')
-    #glueball(0.30, path+'AATmax0p3')
-    #glueball(0.250,  path+'QCD_AATmax0p25')
+    #glueball(0.50, '../results/SU3_T0p5/')
+    glueball(0.60, '../results/SU3_T0p6/')
+    glueball(0.40, '../results/SU3_T0p4/')
+    glueball(0.30, '../results/SU3_T0p3/')
