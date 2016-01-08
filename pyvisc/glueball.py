34df56b86dacdf9dd9857fb4840f2617ccbf34c4
LongGangPang <lpang@lxi076.gsi.de>
2016-01-08 16:40:55 +0100

    add description for EOS in hydro.info; su3 test in glueball.py

--- pyvisc/glueball.py
+++ pyvisc/glueball.py
@@ -24,8 +24,8 @@ def glueball(Tmax = 0.6, outdir = '../results/event0'):
 
     # set IEOS = 2 for (2+1)-flavor QCD EOS
     # set IEOS = 3 for GlueBall EOS
-    cfg.NX = 401
-    cfg.NY = 401
+    cfg.NX = 501
+    cfg.NY = 501
     cfg.NZ = 1
     cfg.DT = 0.01
     cfg.DX = 0.08
@@ -34,25 +34,29 @@ def glueball(Tmax = 0.6, outdir = '../results/event0'):
     cfg.Ra = 6.62
     cfg.Eta = 0.546
     cfg.Si0 = 6.4
-    cfg.TAU0 = 0.5
-    cfg.ImpactParameter = 3.0
+    cfg.TAU0 = 0.2
+    cfg.ImpactParameter = 7.74
     cfg.ETAOS = 0.0
     cfg.SQRTS = 2760
+
+    #cfg.Edmax = 600.0
+    cfg.Hwn = 1.0
     write_config(cfg)
 
-    ideal = CLIdeal(cfg)
+    ideal = CLIdeal(cfg, gpu_id=2)
     from glauber import Glauber
     Glauber(cfg, ideal.ctx, ideal.queue, ideal.gpu_defines,
                   ideal.d_ev[1])
 
-    ideal.evolve(max_loops=1000, save_hypersf=False, to_maxloop=True)
+    ideal.evolve(max_loops=3000, save_hypersf=False, to_maxloop=True)
     t1 = time()
     print('finished. Total time: {dtime}'.format(dtime = t1-t0 ))
 
 
 
 if __name__=='__main__':
-    #glueball(0.50, '../results/SU3_T0p5/')
-    glueball(0.60, '../results/SU3_T0p6/')
-    glueball(0.40, '../results/SU3_T0p4/')
-    glueball(0.30, '../results/SU3_T0p3/')
+    #glueball(0.60, '../results/IdealGas_Ed30/')
+    #glueball(0.50, '../results/IdealGas_T0p5/')
+    #glueball(0.40, '../results/IdealGas_T0p4/')
+    #glueball(0.30, '../results/IdealGas_T0p3/')
+    glueball(0.60, '../results/CompareWithHarri_SU3_T0p6/')
