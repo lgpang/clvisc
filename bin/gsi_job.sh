a77018a743611bbab7b94bafe49edc0bdf2d2a25
LongGangPang <lpang@lxlcsc0001.gsi.de>
2016-03-07 11:43:42 +0100

    use 4 gpus on gsi cluster per node with gsi_job.sh

--- bin/gsi_job.sh
+++ bin/gsi_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash                                                                                           
-#SBATCH --gres=gpu:2                                                                                  
+#SBATCH --gres=gpu:4                                                                                  
 #SBATCH --constraint=hawaii                                                                           
-#SBATCH -D /lustre/nyx/hyihp/lpang/auau39_results/
-#SBATCH --ntasks=1                                                                                    
+#SBATCH -D /lustre/nyx/hyihp/lpang/PyVisc/bin/
+#SBATCH --ntasks=4                                                                                    
 #SBATCH --cpus-per-task=1                                                                             
 #SBATCH --error=log/%a_%j.err                                                                         
 #SBATCH --output=log/%a_%j.out                                                                        
@@ -10,7 +10,7 @@
 #SBATCH --mem-per-cpu=4096                                                                           
 #SBATCH --mail-type=ALL                                                                               
 #SBATCH --partition=lcsc                                                                              
-#SBATCH --time=24:00:00                                                                               
+#SBATCH --time=48:00:00                                                                               
 
 echo "Start time: $date"
 
@@ -18,6 +18,16 @@ unset DISPLAY
 #module load python/2.7                                                                               
 #module load amdappsdk/2.9                                                                            
 
-python ebe_hdf5_auau39.py
+export PATH="/lustre/nyx/hyihp/lpang/anaconda/bin:$PATH"
+export PYTHONPATH="/lustre/nyx/hyihp/lpang/anaconda/lib/python/:$PYTHONPATH"
+export TMPDIR="/lustre/nyx/hyihp/lpang/tmp/"
+
+#modify the cache.py in anaconda/pyopencl and set the cache_dir = TMPDIR
+
+python ebe.py auau200 20_50 0.08 0 &
+python ebe.py auau62p4 20_50 0.08 1 &
+python ebe.py auau39 20_50 0.08 2 &
+python ebe.py auau19p6 20_50 0.08 3 &
+wait
 
 echo "End time: $date"
