ad413be6699c306a8a97307b414d5c4bf07316e9
LongGangPang <lpang@lxlcsc0001.gsi.de>
2016-03-08 10:26:53 +0100

    add ebe script for hdf5 initial conditions; call script with collisions system, centraility, etaos, gpuid

--- polarization/integrated_pol.py
+++ polarization/integrated_pol.py
@@ -14,14 +14,13 @@ import matplotlib.pyplot as plt
 import os
 import h5py
 
-rapidity = np.linspace(-5, 5, 11, endpoint=True)
 
-def init_momentum(f_h5):
+def init_momentum(f_h5, rapidity):
     dset_pt = f_h5.create_dataset('mom/PT', data=mom.PT)
     dset_phi = f_h5.create_dataset('mom/PHI', data=mom.PHI)
     dset_rapidity = f_h5.create_dataset('mom/Y', data=rapidity)
 
-def integrated_polarization(f_h5, fpath, event_id):
+def integrated_polarization(f_h5, fpath, event_id, rapidity):
     '''calc the pt, phi integrated lambda polarization as a function of
     rapidity.
     The results is stored in hdf5 file'''
@@ -59,28 +58,36 @@ def integrated_polarization(f_h5, fpath, event_id):
 
 
 
-def create_file(fname='vor_auau62p4.hdf5'):
+def create_file(fname, rapidity):
     f_h5 = h5py.File(fname, 'w')
-    init_momentum(f_h5)
+    init_momentum(f_h5, rapidity)
     return f_h5
 
-def update_h5(start_id, end_id, f_h5name, path, create=False):
+def update_h5(start_id, end_id, f_h5name, path, rapidity, create=False):
     # store the data in hdf5 file
     f_h5 = None
-    if not os.path.exists(f_h5name):
-        f_h5 = create_file(f_h5name)
+    if not os.path.exists(f_h5name) or create:
+        f_h5 = create_file(f_h5name, rapidity)
     else:
         f_h5 = h5py.File(f_h5name, 'r+')
 
     for event_id in range(start_id, end_id):
         fpath = '%s/event%s'%(path, event_id)
-        integrated_polarization(f_h5, fpath, event_id)
+        integrated_polarization(f_h5, fpath, event_id, rapidity)
         print('event', event_id, 'finished')
     f_h5.close()
 
 
 
-if __name__ == '__main__':
-    f_h5name = 'vor_int_visc0p12_auau62p4_cent45_50.hdf5'
-    path = '/lustre/nyx/hyihp/lpang/auau62p4_results/cent45_50/etas0p12/'
-    update_h5(62, 96, f_h5name, path, create=False)
+def fin_grid_mid_rapidity():
+    #f_h5name = 'vor_int_visc0p12_auau62p4_cent45_50.hdf5'
+    #path = '/lustre/nyx/hyihp/lpang/auau62p4_results/cent45_50/etas0p12/'
+    #update_h5(62, 96, f_h5name, path, create=False)
+
+    rapidity = np.linspace(-0.9, 0.9, 10, endpoint=True)
+    f_h5name = 'vor_int_visc0p12_auau62p4_cent20_25_mid_rapidity.hdf5'
+    path = '/lustre/nyx/hyihp/lpang/auau62p4_results/cent20_25/etas0p12/'
+    update_h5(0, 93, f_h5name, path, rapidity, create=True)
+
+
+fin_grid_mid_rapidity()
