b512eaa751da95c8808c8553ef7bdb9dc3b91152
LongGangPang <lpang@lxi073.gsi.de>
2016-07-30 14:48:46 +0200

    change script for Glueball

--- pyvisc/glueball.py
+++ pyvisc/glueball.py
@@ -13,37 +13,55 @@ from config import cfg, write_config
 import numpy as np
 import matplotlib.pyplot as plt
 
-def glueball(Tmax = 0.6, outdir = '../results/event0'):
+def glueball(Tmax = 0.6, outdir = '../results/event0', IEOS=3):
     print('start ...')
     t0 = time()
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    cfg.IEOS = 3
+    cfg.IEOS = IEOS 
     eos = Eos(cfg.IEOS)
     # update the configuration
-    cfg.Edmax = eos.f_ed(Tmax)
+    #cfg.Edmax = eos.f_ed(Tmax)
+    #cfg.Edmax = 166.0
+    cfg.Edmax = 55.0
+
     cfg.fPathOut = outdir
 
     # set IEOS = 2 for (2+1)-flavor QCD EOS
     # set IEOS = 3 for GlueBall EOS
+
     cfg.NX = 501
     cfg.NY = 501
     cfg.NZ = 1
     cfg.DT = 0.01
     cfg.DX = 0.08
     cfg.DY = 0.08
-    cfg.A = 208
-    cfg.Ra = 6.62
+
+    #cfg.A = 208
+    #cfg.Ra = 6.62
+    #cfg.Eta = 0.546
+    #cfg.Si0 = 6.4
+
+    cfg.A = 197
+    cfg.Ra = 6.4
     cfg.Eta = 0.546
-    cfg.Si0 = 6.4
-    cfg.TAU0 = 0.2
+    cfg.Si0 = 4.0
+
+    cfg.TAU0 = 0.4
     cfg.ImpactParameter = 7.74
+
+    #cfg.ImpactParameter = 0.0
     cfg.ETAOS = 0.0
-    cfg.SQRTS = 2760
+
+    cfg.save_to_hdf5 = False
+
+    if IEOS == 3:
+        cfg.TFRZ = 0.2
 
     #cfg.Edmax = 600.0
-    cfg.Hwn = 1.0
+    #cfg.Hwn = 1.0
+    cfg.Hwn = 0.95
     write_config(cfg)
 
     ideal = CLIdeal(cfg, gpu_id=2)
@@ -54,6 +72,8 @@ def glueball(Tmax = 0.6, outdir = '../results/event0'):
     t1 = time()
     print('finished. Total time: {dtime}'.format(dtime = t1-t0 ))
 
+    from subprocess import call
+    call(['python', './spec.py', cfg.fPathOut])
 
 
 def pp_energydensity(x, y, b):
@@ -140,7 +160,11 @@ def ppcollision(eostype='SU3', outdir = '../results/event0'):
 
 
 if __name__=='__main__':
-    #glueball(0.60, '../results/IdealGas_Ed30/')
+    #glueball(0.60, '../results/Glueball_T0p6/', IEOS=3)
+    #glueball(0.60, '../results/Lattice2p1_T0p6/', IEOS=2)
+    glueball(outdir='../results/Glueball_Edmax55/', IEOS=3)
+    glueball(outdir='../results/Lattice2p1_Edmax55/', IEOS=2)
+
     #glueball(0.50, '../results/IdealGas_T0p5/')
     #glueball(0.40, '../results/IdealGas_T0p4/')
     #glueball(0.30, '../results/IdealGas_T0p3/')
@@ -148,5 +172,5 @@ if __name__=='__main__':
     #ppcollision(eostype='SU3', outdir='../results/PP_SU3')
     #ppcollision(eostype='QCD', outdir='../results/PP_QCD')
     #ppcollision(eostype='EOSI', outdir='../results/PP_EOSI')
-    ppcollision(eostype='EOSI', outdir='../results/PP_EOSI_1')
+    #ppcollision(eostype='EOSI', outdir='../results/PP_EOSI_1')
 
