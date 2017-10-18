67cf66981bb0eb883ae2ae90ad15903d40675b1f
LongGang Pang <lgpang@qq.com>
2017-10-18 11:40:52 -0700

    update the dndeta/ptspec/vn calculation

--- sampler/mcspec/sampler.py
+++ sampler/mcspec/sampler.py
@@ -35,10 +35,12 @@ def get_ptspec(fpath, dat, pid=211, nsampling=2000, kind='Y', rapidity_window=1.
     if kind == 'Eta':
         rapidity_col = 6
 
-    particle_type = (dat[:, 5]==pid)
+    particle_type = None
 
     if pid == 'charged':
         particle_type = (dat[:, 5]==dat[:, 5])
+    else:
+        particle_type = (dat[:, 5]==pid)
 
     Yi = dat[particle_type, rapidity_col]
 
@@ -62,14 +64,17 @@ def get_ptspec(fpath, dat, pid=211, nsampling=2000, kind='Y', rapidity_window=1.
     np.savetxt(fname, res)
     #return res[:, 0], res[:, 1]
 
-def get_event_planes(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=3.3, Yhi=3.9, total_n=6):
-#    phi_p = np.arctan2(dat[:,1],dat[:,2])
+def get_event_planes(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=3.3, Yhi=3.9, total_n=6,
+                     num_ptbins=15, num_phibins=50):
     rapidity_col = 4
     if kind == 'Eta':
         rapidity_col = 6
-    particle_type = (dat[:,5]==pid)
+
+    particle_type = None
     if pid == 'charged':
         particle_type = (dat[:, 5]==dat[:, 5])
+    else:
+        particle_type = (dat[:, 5]==pid)
 
     Yi = dat[particle_type, rapidity_col]
     phi_p = np.arctan2(dat[particle_type,2], dat[particle_type,1])
@@ -77,14 +82,14 @@ def get_event_planes(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=3.3, Yhi=
 
     pti = pti[(Yi>Ylo)*(Yi<Yhi)]
     phi_p = phi_p[(Yi>Ylo)*(Yi<Yhi)] 
-    d2N, pt, Phi = np.histogram2d(pti, phi_p, bins=50) 
+    d2N, pt, Phi = np.histogram2d(pti, phi_p, range=[[0, 4.0], [-np.pi, np.pi]], bins=[num_ptbins, num_phibins]) 
     dpt = pt[1:]-pt[:-1] 
     dphi_p = Phi[1:]-Phi[:-1]
     pt = 0.5*(pt[1:]+pt[:-1])
     Phi = 0.5*(Phi[1:]+Phi[:-1]) 
     
     d2N=d2N.flatten()  
-    Phi = np.repeat(Phi,50)
+    Phi = np.repeat(Phi, num_ptbins)
     
     Vn = np.zeros(total_n+1)
     event_plane = np.zeros(total_n+1)
@@ -93,49 +98,45 @@ def get_event_planes(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=3.3, Yhi=
     print('event_plane_window',Norm,pid)
     for n in range(1, total_n+1):
         total_vn[n] = (d2N * np.exp(1j*n*Phi)).sum()/float(Norm)
-        #for k in range(0, 50*50):
-        #    total_vn[n] += d2N[k]*np.exp(1j*n*Phi[k])/float(Norm)
         Vn[n], event_plane[n]=cmath.polar(total_vn[n])
         event_plane[n] /= float(n)
     return event_plane
 
-def get_vn_pt(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=-0.35, Yhi=0.35, Y_event_plane=[3.3, 3.9], total_n=6):
+def get_vn_pt(fpath, dat, pid=211, nsampling=2000, kind='Y',Ylo=-0.35, Yhi=0.35,
+        Y_event_plane=[3.3, 3.9], total_n=6, num_ptbins=15, num_phibins=50):
     rapidity_col = 4
     if kind == 'Eta':
         rapidity_col = 6
-    particle_type = (dat[:, 5]==pid)
+    particle_type = None 
     if pid == 'charged':
         particle_type = (dat[:, 5]==dat[:, 5])
+    else:
+        particle_type = (dat[:, 5]==pid)
 
     Yi = dat[particle_type, rapidity_col]
     phi_p = np.arctan2(dat[particle_type,2], dat[particle_type,1])
     pti = np.sqrt(dat[particle_type, 1]**2 + dat[particle_type, 2]**2)
     pti = pti[np.abs(Yi)<Yhi]
     phi_p = phi_p[np.abs(Yi)<Yhi]
-    d2N, pt, Phi = np.histogram2d(pti, phi_p, bins=50)
+    d2N, pt, Phi = np.histogram2d(pti, phi_p, range=[[0, 4.0], [-np.pi, np.pi]], bins=[num_ptbins, num_phibins])
     dpt = pt[1:]-pt[:-1]
     dphi_p = Phi[1:]-Phi[:-1]
     pt = 0.5*(pt[1:]+pt[:-1])
     Phi = 0.5*(Phi[1:]+Phi[:-1])
     
-    Vn_pt = np.zeros(shape=(50, total_n+1))
-    Vn_vec = np.zeros(shape=(50, total_n+1), dtype=complex)
-    angles = np.zeros(shape=(50, total_n+1))
+    Vn_pt = np.zeros(shape=(num_ptbins, total_n+1))
+    Vn_vec = np.zeros(shape=(num_ptbins, total_n+1), dtype=complex)
+    angles = np.zeros(shape=(num_ptbins, total_n+1))
     event_plane = np.zeros(total_n+1)
     Norm = 0
     event_plane = get_event_planes(fpath,dat,pid,nsampling,kind, Ylo=Y_event_plane[0], Yhi=Y_event_plane[1])
-    for i in range(50):
+    for i in range(num_ptbins):
         norm_factor = np.sum(d2N[i,:])
-        if norm_factor>1e-2:
-            print('norm,pti',norm_factor,pt[i])
-            for n in range(1, 7):
-                Vn_vec[i, n] = (d2N[i]*np.exp(1j*n*(Phi-event_plane[n]))).sum()/float(norm_factor)
-                #  for k in range(0, 50):
-                #        Vn_vec[i, n-1] += d2N[i,k]*np.exp(1j*n*(Phi[k]-event_plane[n-1]))/float(norm_factor)
-                Vn_pt[i, n], angles[i, n]=cmath.polar(Vn_vec[i, n])
-        else:
-            Vn_vec[i,:] = 0
-            Vn_pt[i,:] = 0  
+        if norm_factor<1e-2: continue
+        print('norm,pti',norm_factor,pt[i])
+        for n in range(1, 7):
+            Vn_vec[i, n] = (d2N[i]*np.exp(1j*n*(Phi-event_plane[n]))).sum()/float(norm_factor)
+            Vn_pt[i, n], angles[i, n]=cmath.polar(Vn_vec[i, n])
     fout_name = os.path.join(fpath,'vn_mc_%s.dat'%pid)
     np.savetxt(fout_name, np.array(list(zip(pt, Vn_pt[:,1],Vn_pt[:,2],Vn_pt[:,3],Vn_pt[:,4],Vn_pt[:,5], Vn_pt[:,6]))))
 
