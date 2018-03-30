7edbd1272b5c7ff31aedf82ebce52e78962045d4
LongGangPang <lpang@lxlcsc0001.gsi.de>
2018-03-30 19:04:07 +0200

    remove smooth spectra from plain cpp calc; redudant code  clearing

--- CLSmoothSpec/src/main.cpp
+++ CLSmoothSpec/src/main.cpp
@@ -58,94 +58,94 @@ namespace {
 
 } // end unnamed namespace
 
-void check_hadrons(const Spec & spec, std::string DataPath) {
-    int pid = 1;
-    char fname[256];
-    double scale_factor = 1000;
-
-    //#pragma omp parallel for
-    for (auto h : spec.h_HadronInfo) {
-        if (h.s[0] < 0.001) continue;
-        double mass = h.s[0];
-        double gspin = h.s[1];
-        double fermi_boson = h.s[2];
-        double muB = h.s[3];
-        std::cout << "spec_name = " << spec.particles.at(pid).name << " finished" << std::endl;
-        std::cout << "spec_monval = " << spec.particles.at(pid).monval << std::endl;
-        std::cout << "spec_mass = " << spec.particles.at(pid).mass << std::endl;
-        std::cout << h.s[0] << " " << h.s[1] << " " << h.s[2] << " " << h.s[3] << std::endl;
-        pid ++;
-    }
-} // end check_hadrons
-
-void spec_cpp(const Spec & spec, std::string DataPath) {
-    int pid = 1;
-    char fname[256];
-    double scale_factor = 1000;
-
-    //#pragma omp parallel for
-    for (auto h : spec.h_HadronInfo) {
-        if (h.s[0] < 0.001) continue;
-        std::cout << h.s[0] << " " << h.s[1] << " " << h.s[2] << " " << h.s[3] << std::endl;
-        double mass = h.s[0];
-        double gspin = h.s[1];
-        double fermi_boson = h.s[2];
-        double muB = h.s[3];
-        double dof = gspin / std::pow(2.0*M_PI, 3.0);
-        double dNdYPtdPtdPhi[NY][NPT][NPHI];
-        double dNdEtaPtdPtdPhi[NY][NPT][NPHI];
-        for( int k=0; k!=NY; k++ )
-            for( int l=0; l!=NPT; l++ )
-                for( int m=0; m!=NPHI; m++ ){
-                    dNdYPtdPtdPhi[k][l][m] = 0.0;
-                }
-
-        std::cout << "initialize finished !" << std::endl;
-
-        for (const auto & SF : spec.h_SF) {
-            double ds0 = SF.s[0];
-            double ds1 = SF.s[1];
-            double ds2 = SF.s[2];
-            double ds3 = SF.s[3];
-            double vx  = SF.s[4];
-            double vy  = SF.s[5];
-            double vh  = SF.s[6];
-            double etas  = SF.s[7];
-            double gamma = 1.0/std::sqrt(std::max(1.0E-30, 1.0-vx*vx - vy*vy - vh*vh));
-            for (int k=0; k!=NY; k++)
-                for (int l=0; l!=NPT; l++){
-                    double pt =  spec.h_Pt[l];
-                    double mt = std::sqrt(mass*mass + pt*pt);
-                    double mtcy =  mt * cosh( spec.h_Y[k] - etas );
-                    double mtsy =  mt * sinh( spec.h_Y[k] - etas );
-                    for( int m=0; m!=NPHI; m++ ){
-                        double  px = pt *  spec.h_CPhi[m];
-                        double  py = pt *  spec.h_SPhi[m];
-                        double efkt = ( gamma * ( mtcy - px*vx - py*vy - mtsy*vh) - muB ) / spec.Tfrz;
-                        double volum = mtcy*ds0 - px*ds1 - py*ds2 - mtsy*ds3;
-                        dNdYPtdPtdPhi[k][l][m] += dof * volum / ( std::exp(efkt) + fermi_boson ) * scale_factor;
-                        /** in units of GeV.fm^3 */
-                    }
-                }
-        }
-        //std::cout << '.';
-        std::cout << "cpp spec for " << spec.particles.at(pid).name << " finished" << std::endl;
-
-        int monval = spec.particles.at(pid).monval;
-        // write spec in Y, Pt, Phi
-        if( monval > 0 ) sprintf( fname, "%s/dNdYPtdPtdPhi_Reso%d.dat", DataPath.c_str(), monval );
-        else sprintf( fname, "%s/dNdYPtdPtdPhi_ResoA%d.dat", DataPath.c_str(), -monval );
-        WriteSpec(fname, dNdYPtdPtdPhi);
-
-        // write spec in Eta, Pt, Phi
-        get_dNdEtaPtdPtdPhi(spec, mass, dNdYPtdPtdPhi, dNdEtaPtdPtdPhi);
-        if( monval > 0 ) sprintf( fname, "%s/dNdEtaPtdPtdPhi_Reso%d.dat", DataPath.c_str(), monval );
-        else sprintf( fname, "%s/dNdEtaPtdPtdPhi_ResoA%d.dat", DataPath.c_str(), -monval );
-        WriteSpec(fname, dNdEtaPtdPtdPhi);
-
-        pid++;
-    }
-}
+//void check_hadrons(const Spec & spec, std::string DataPath) {
+//    int pid = 1;
+//    char fname[256];
+//    double scale_factor = 1000;
+//
+//    //#pragma omp parallel for
+//    for (auto h : spec.h_HadronInfo) {
+//        if (h.s[0] < 0.001) continue;
+//        double mass = h.s[0];
+//        double gspin = h.s[1];
+//        double fermi_boson = h.s[2];
+//        double muB = h.s[3];
+//        std::cout << "spec_name = " << spec.particles.at(pid).name << " finished" << std::endl;
+//        std::cout << "spec_monval = " << spec.particles.at(pid).monval << std::endl;
+//        std::cout << "spec_mass = " << spec.particles.at(pid).mass << std::endl;
+//        std::cout << h.s[0] << " " << h.s[1] << " " << h.s[2] << " " << h.s[3] << std::endl;
+//        pid ++;
+//    }
+//} // end check_hadrons
+//
+//void spec_cpp(const Spec & spec, std::string DataPath) {
+//    int pid = 1;
+//    char fname[256];
+//    double scale_factor = 1000;
+//
+//    //#pragma omp parallel for
+//    for (auto h : spec.h_HadronInfo) {
+//        if (h.s[0] < 0.001) continue;
+//        std::cout << h.s[0] << " " << h.s[1] << " " << h.s[2] << " " << h.s[3] << std::endl;
+//        double mass = h.s[0];
+//        double gspin = h.s[1];
+//        double fermi_boson = h.s[2];
+//        double muB = h.s[3];
+//        double dof = gspin / std::pow(2.0*M_PI, 3.0);
+//        double dNdYPtdPtdPhi[NY][NPT][NPHI];
+//        double dNdEtaPtdPtdPhi[NY][NPT][NPHI];
+//        for( int k=0; k!=NY; k++ )
+//            for( int l=0; l!=NPT; l++ )
+//                for( int m=0; m!=NPHI; m++ ){
+//                    dNdYPtdPtdPhi[k][l][m] = 0.0;
+//                }
+//
+//        std::cout << "initialize finished !" << std::endl;
+//
+//        for (const auto & SF : spec.h_SF) {
+//            double ds0 = SF.s[0];
+//            double ds1 = SF.s[1];
+//            double ds2 = SF.s[2];
+//            double ds3 = SF.s[3];
+//            double vx  = SF.s[4];
+//            double vy  = SF.s[5];
+//            double vh  = SF.s[6];
+//            double etas  = SF.s[7];
+//            double gamma = 1.0/std::sqrt(std::max(1.0E-30, 1.0-vx*vx - vy*vy - vh*vh));
+//            for (int k=0; k!=NY; k++)
+//                for (int l=0; l!=NPT; l++){
+//                    double pt =  spec.h_Pt[l];
+//                    double mt = std::sqrt(mass*mass + pt*pt);
+//                    double mtcy =  mt * cosh( spec.h_Y[k] - etas );
+//                    double mtsy =  mt * sinh( spec.h_Y[k] - etas );
+//                    for( int m=0; m!=NPHI; m++ ){
+//                        double  px = pt *  spec.h_CPhi[m];
+//                        double  py = pt *  spec.h_SPhi[m];
+//                        double efkt = ( gamma * ( mtcy - px*vx - py*vy - mtsy*vh) - muB ) / spec.Tfrz;
+//                        double volum = mtcy*ds0 - px*ds1 - py*ds2 - mtsy*ds3;
+//                        dNdYPtdPtdPhi[k][l][m] += dof * volum / ( std::exp(efkt) + fermi_boson ) * scale_factor;
+//                        /** in units of GeV.fm^3 */
+//                    }
+//                }
+//        }
+//        //std::cout << '.';
+//        std::cout << "cpp spec for " << spec.particles.at(pid).name << " finished" << std::endl;
+//
+//        int monval = spec.particles.at(pid).monval;
+//        // write spec in Y, Pt, Phi
+//        if( monval > 0 ) sprintf( fname, "%s/dNdYPtdPtdPhi_Reso%d.dat", DataPath.c_str(), monval );
+//        else sprintf( fname, "%s/dNdYPtdPtdPhi_ResoA%d.dat", DataPath.c_str(), -monval );
+//        WriteSpec(fname, dNdYPtdPtdPhi);
+//
+//        // write spec in Eta, Pt, Phi
+//        get_dNdEtaPtdPtdPhi(spec, mass, dNdYPtdPtdPhi, dNdEtaPtdPtdPhi);
+//        if( monval > 0 ) sprintf( fname, "%s/dNdEtaPtdPtdPhi_Reso%d.dat", DataPath.c_str(), monval );
+//        else sprintf( fname, "%s/dNdEtaPtdPtdPhi_ResoA%d.dat", DataPath.c_str(), -monval );
+//        WriteSpec(fname, dNdEtaPtdPtdPhi);
+//
+//        pid++;
+//    }
+//}
 
 /////////////////////////////////////////////////
 int main(int argc, char** argv)
